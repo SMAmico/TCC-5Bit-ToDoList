aeee937dda23c7c940a64fe0f72386eab03cf84e
SMAmico <91100586+SMAmico@users.noreply.github.com>
2023-12-15 14:26:20 -0800

    finalization and documentation

--- src/GroupProjectJamesNew.java
+++ src/GroupProjectJamesNew.java
@@ -1,4 +1,9 @@
-import java.util.ArrayList;
+
+/*
+ * GroupProjectJamesNew(thrilling name I know) is designed to utilize the methods and 
+ * structure ToDoList.java from the extra file. this provides a UI and applications/ management. 
+ * both ToDoList.java and GroupProjectJamesNew.java are in the same default package so no imports needed.
+ */
 import java.util.Scanner;
 
 public class GroupProjectJamesNew {
@@ -13,8 +18,8 @@ public class GroupProjectJamesNew {
 		// -------------------------------------------
 		Scanner scanner = new Scanner(System.in);
 		String input = "";
-
-		while (!input.equals("5")) {
+		boolean running = true;
+		while (running) {
 			// System.out.println("--------------------------------"); for style if you
 			// wanted to add
 			System.out.println("Please select an option number:");
@@ -27,65 +32,66 @@ public class GroupProjectJamesNew {
 			System.out.println("5. Exit");
 			System.out.println("--------------------------------");
 
-			input = scanner.nextLine();
+			input = scanner.nextLine();// fetch the user choice
 
 			if (input.equals("1")) {
 				System.out.println("--------------------------------");
 				System.out.println("Enter a name for the task:");
 				System.out.println("--------------------------------");
-				String title = scanner.nextLine();
+				String title = scanner.nextLine();// take all three fields
 				System.out.print(" enter some info about it : ");
 				String content = scanner.nextLine();
 				System.out.print(" next, enter a due date : ");
 				String deadline = scanner.nextLine();
-				if (tasks.addNode(title, content, deadline)) {
+				if (tasks.addNode(title, content, deadline)) {// create an object with those definitions
 					System.out.println("--------------------------------");
 					System.out.println("Task added.");
 					System.out.println("--------------------------------");
 				}
 			} else if (input.equals("2")) {
+				System.out.println("--------------------------------");
+				System.out.println("Enter the index of the task to remove (from 1) :");
+				System.out.println("--------------------------------");
+				int index = scanner.nextInt();
+				scanner.nextLine(); // consume newline left-over
+				index--; // adjust index to start from 0
+				try { // if the user input or program input is incorrect, catch any exceptions that
+						// occur and continue the program.
+					Node taskDeleted = tasks.getTask(index);// fetch task before it is removed
+					tasksDone.addNode(taskDeleted.title, taskDeleted.content, taskDeleted.deadline);
+					tasks.removeNode(index);// ONLY remove node after the deleted node has been successfully added to
+											// history
+					System.out.println("Task removed.");
 					System.out.println("--------------------------------");
-					System.out.println("Enter the index of the task to remove:");
+				} catch (IndexOutOfBoundsException R) {// catch numbers beyond list
 					System.out.println("--------------------------------");
-					int index = scanner.nextInt();
-					scanner.nextLine(); // consume newline left-over
-					index--; // adjust index to start from 0
-					Node taskDeleted = tasks.getTask(index);
-					if (taskDeleted != null) {
-						tasksDone.addNode(taskDeleted.title, taskDeleted.content, taskDeleted.deadline);
-						tasks.removeNode(index);
-						System.out.println("Task removed.");
-						System.out.println("--------------------------------");
-					} else {
-						System.out.println("--------------------------------");
-						System.out.println("Task not found or does not Exist.");
-						System.out.println("--------------------------------");
-						throw new IllegalArgumentException("No index was found or does not exist!");
+					System.out.println("there is no accessible task at that index.");
+					System.out.println("--------------------------------");
+				}
+			} else if (input.equals("3")) {
+				System.out.println("--------------------------------");
+				System.out.println("All tasks:");
+				System.out.println("--------------------------------");
+				if (!tasks.isEmpty()) {
+					for (int i = 0; i < tasks.size(); i++) {
+						Node listTask = tasks.getTask(i);
+						// Check if the task is null before trying to access its properties(saves an unneeded exception)
+						if (listTask != null) {
+							System.out.println("|Task:");
+							System.out.println("| " + (i + 1) + ". " + listTask.title);
+							System.out.println("|    :" + listTask.content);
+							System.out.println("|        Due by: " + listTask.deadline);
+							System.out.println("--------------------------------");
+						}
 					}
-				} else if (input.equals("3")) {
-				    System.out.println("--------------------------------");
-				    System.out.println("All tasks:");
-				    System.out.println("--------------------------------");
-				    if (!tasks.isEmpty()) {
-				        for (int i = 0; i < tasks.size(); i++) {
-				            Node listTask = tasks.getTask(i);
-				            // Check if the task is null before trying to access its properties
-				            if (listTask != null) {
-				                System.out.println("|Task:");
-				                System.out.println("| " + (i + 1) + ". " + listTask.title);
-				                System.out.println("|    :" + listTask.content);
-				                System.out.println("|        Due by: " + listTask.deadline);
-				                System.out.println("--------------------------------");
-				            }
-				        }
-				        System.out.println(" ~end of todo list! ~");
-						System.out.println("--------------------------------");
+					System.out.println(" ~end of todo list! ~");
+					System.out.println("--------------------------------");
 
-				    } else {
-				        System.out.println(" List is empty! ");
-						System.out.println("--------------------------------");
+				} else {
+					System.out.println(" List is empty! ");
+					System.out.println("--------------------------------");
 
-				    }
+				}
 			} else if (input.equals("4")) {
 				System.out.println("--------------------------------");
 				System.out.println("Here's what you've completed:");
@@ -97,7 +103,7 @@ public class GroupProjectJamesNew {
 						System.out.println("| " + (i + 1) + ". " + listTask.title);
 						System.out.println("|    :" + listTask.content);
 						System.out.println("|        Due by: " + listTask.deadline);
-						System.out.println("|--------------------------------");
+						System.out.println("--------------------------------");
 					}
 					System.out.println(" ~ that's all you've done! look at you go! ~");
 					System.out.println("--------------------------------");
@@ -111,11 +117,16 @@ public class GroupProjectJamesNew {
 				System.out.println("--------------------------------");
 				System.out.println("Exiting.");
 				System.out.println("--------------------------------");
+				scanner.close();//eclipse was mad about this
+				running = false;//cuts the program while statement
 			} else {
 				System.out.println("--------------------------------");
 				System.out.println("Invalid option, try again.");
 				System.out.println("--------------------------------");
-				throw new IllegalArgumentException("Not one of the choices, enter a numbers between 1 and 5.");
+				// throw new IllegalArgumentException("Not one of the choices, enter a numbers
+				// between 1 and 5."); THIS WAS THE OLD EXCEPTION THROWER, now has been turned
+				// into a not-frag grenade. it should continue the program despite incorrect
+				// input
 			}
 		}
 	}
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -10,126 +10,174 @@
 8.Create method to remove a task  -hjam
 9.Create method to display the current tasks -seth
 10.Create method to view completed task history -seth
-*/ 
+*/
 
-class Node{
+/*
+ * ToDoList class defines a Doubly linked list object. it has the ability to store three strings, access 
+ * and return any node in the list, remove any node in the list, and return data for any extracted node. 
+ * additionally, there is an O(1) size and an isEmpty checker.
+ */
+
+class Node {// node object definition
 	String content;
 	String title;
 	String deadline;
 	Node prev;
 	Node next;
-	
-	
-	public Node(String title, String content, String deadline) {
+
+	public Node(String title, String content, String deadline) {// individual node constructor, O(1)
 		this.content = content;
 		this.title = title;
 		this.deadline = deadline;
-
 		this.prev = null;
 		this.next = null;
 	}
 }
 
-class ToDoList {
+class ToDoList {// creates a todolist object with a head and a tail node, as well as an overall
+				// size
 	Node head;
 	Node tail;
 	private int size = 0;
+
 //this comment is just a test for my pc being able to edit files and push properly.
-	//another test
-	//Test
-	public boolean addNode(String title, String content, String deadline) {
+	// another test
+	// Test
+	public boolean addNode(String title, String content, String deadline) {// hiba's original code. adds a node within
+																			// the todolist object. O(1)
 		Node newNode = new Node(title, content, deadline);
-			if (head == null) {
-				head = newNode;
-			} else {
-				tail.next = newNode;
-				newNode.prev = tail;
-			}
-			tail = newNode;
-			size++;
-			return true;
-	}
-	
-	public boolean removeNode(int index) {
-		if (head == null || index < 0) {
-				return false;
-			}
-			if (index == 0) {
-				head = head.next;
-				if (head != null) {
-					head.prev = null;
-				} else {
-					tail = null;
-				}
-				return true;
-			}
-			Node current = head;
-			int currentIndex = 0;
-
-			while (current != null && currentIndex < 0) {
-				current = current.next;
-				currentIndex++;
-			}
-
-			if (current == null) {
-				// index out of bounds
-				return false;
-			}
-			// Remove the node
-			Node prevNode = current.prev;
-			Node nextNode = current.next;
-
-			if (prevNode != null) {
-				prevNode.next = nextNode;
-			} else {
-				head = nextNode;
-			}
-			if (nextNode != null) {
-				nextNode.prev = prevNode;
-			} else {
-				tail = prevNode;
-			}
-			size--;
-			return true;
+		if (head == null) {
+			head = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.prev = tail;
 		}
-	    
-	public double size() {
+		tail = newNode;
+		size++;
+		return true;
+	}
+
+	public boolean removeNode(int index) {// O(n)
+		// this was original code from hiba. we couldn't debug it properly so we had to
+		// replace it.
+//		if (head == null || index < 0 || index > size) {
+//				return false;
+//			}
+//			if (index == 0) {
+//				head = head.next;
+//				if (head != null) {
+//					head.prev = null;
+//				} else {
+//					tail = null;
+//				}
+//				return true;
+//			}
+//			Node current = head;
+//			int currentIndex = 0;
+//
+//			while (current != null && currentIndex < 0) {
+//				current = current.next;
+//				currentIndex++;
+//			}
+//
+//			if (current == null) {
+//				// index out of bounds
+//				return false;
+//			}
+//			// Remove the node
+//			Node prevNode = current.prev;
+//			Node nextNode = current.next;
+//
+//			if (prevNode != null) {
+//				prevNode.next = nextNode;
+//			} else {
+//				head = nextNode;
+//			}
+//			if (nextNode != null) {
+//				nextNode.prev = prevNode;
+//			} else {
+//				tail = prevNode;
+//			}
+//			size--;
+//			return true;
+
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();// throw an exception if the index isn't in the DLList
+		}
+		Node walker = head;
+		for (int i = 0; i < index; i++) {
+			walker = walker.next;// grab the data before it's dereferenced
+		}
+		if (head == tail) {// if both are the same node, delete their references
+			head = tail = null;
+		} else if (walker == head) {// if it's the head, delete the head and reassign the head to the previous head
+			head = head.next;
+			head.prev = null;
+		} else if (walker == tail) {// if it's the head, delete the head and reassign the head to the previous head
+			tail = tail.prev;
+			tail.next = null;
+		} else {
+			(walker.prev).next = walker.next;// if it's in the middle somewhere, tie the surrounding node's pointers
+												// together
+			(walker.next).prev = walker.prev;
+		}
+
+		size--;// decrease the size and return true
+		return true;
+	}
+
+	public int size() {// returns size. incredible
 		return size;
 	}
-	
-	public Node getTask(int index) {//returns a selected node
-		if(index <= size/2) {
+
+	public Node getTask(int index) {// returns a selected node,O(n)
+//		if(index <= size/2) {
+//		Node walker = head;
+//		for(int i = 0; i < index; i++) {
+//			walker = walker.next;
+//		}
+//		return walker;
+//		} else {
+//			Node walker = tail;
+//			for(int i = size; i > index; i--) {
+//				walker = walker.next;
+//			}
+//			return walker;
+//		}
+		// check for the special case of an out of
+		// bounds index
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		// if we got this far, the index is in bounds
+		// we need to loop to that position in the list
 		Node walker = head;
-		for(int i = 0; i < index; i++) {
+		// loop forwards "index" times
+		for (int i = 0; i < index; i++) {
 			walker = walker.next;
 		}
+		// now walker points to the Node at position "index"
+		// return that node's data
 		return walker;
-		} else {
-			Node walker = tail;
-			for(int i = size; i > index; i--) {
-				walker = walker.next;
-			}
-			return walker;
-		}
 	}
-	
-	public boolean isEmpty() {
-		if(head == null && tail == null && size == 0) {
+
+	public boolean isEmpty() {// returns true if the todolist is empty, O(1)
+		if (head == null && tail == null && size == 0) {
 			return true;
 		}
 		return false;
 	}
-	
-	public String getTitle(Node r) {
+
+	public String getTitle(Node r) {// returns title for a node O(1)
 		return r.title;
 	}
-	
-	public String getContent(Node r) {
+
+	public String getContent(Node r) {// returns content for a node O(1)
 		return r.content;
 	}
-	public String getDeadline(Node r) {
+
+	public String getDeadline(Node r) {// returns deadline/duedate for a node, O(1)
 		return r.deadline;
 	}
-	
-}
 
+}
